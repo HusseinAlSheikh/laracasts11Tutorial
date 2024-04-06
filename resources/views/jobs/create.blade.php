2277d45c37febb13df655696a0a36691fed5466d
Hussein Alsheikh <hussein.jodd@gmail.com>
2024-04-06 13:51:05 +0300

    day 17

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -10,14 +10,17 @@
     @csrf
     <div class="space-y-12">
       <div class="border-b border-gray-900/10 pb-12">
-  
+        
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div class="sm:col-span-4">
             <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input type="text" name="title" id="title" autocomplete="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="do task">
+                <input type="text" name="title" id="title" autocomplete="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="do task" required value="{{ old('title') }}">
               </div>
+              @error('title')
+                 <p class="text-xs text-red-500 font-semibold mt-1"> {{ $message }} </p>
+              @enderror
             </div>
           </div>
 
@@ -25,12 +28,27 @@
             <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input type="text" name="salary" id="salary" autocomplete="salary" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="1,000,000 $">
+                <input type="text" name="salary" id="salary" autocomplete="salary" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="1,000,000 $" required value="{{ old('salary') }}">
               </div>
+              @error('salary')
+                  <p class="text-xs text-red-500 font-semibold mt-1"> {{ $message }} </p>
+              @enderror
             </div>
           </div>
-  
+          
+         
+
         </div>
+        {{-- <div class="mt-10">
+          @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error )
+                    <li class="text-red-500 italic"> {{ $error }} </li>
+                @endforeach
+            </ul>
+          @endif
+        </div> --}}
+          
       </div>
   
   
